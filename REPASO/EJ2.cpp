e08d78366ef6591d0bf3450460a5815cbd85367b
karolhof <2303211@ucc.edu.ar>
2024-08-05 14:40:06 -0300

    implementacion de recursividad

--- REPASO/EJ2.cpp
+++ REPASO/EJ2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int n){
+unsigned long long factorialIterativo(int n){
     int fact = 1;
     for(int i = 1; i <= n; i++){
         fact *= i;
@@ -9,13 +9,22 @@ int factorial(int n){
     return fact;
 }
 
+unsigned long long factorialRecursivo(int n){
+    if (n==0){
+        return 1;
+    } else {
+        return n*factorialRecursivo(n-1);
+    }
+}
+
 int main() {
     
     int n;
     cout<<"Ingrese un numero entero positivo"<<endl;
     cin>>n;
-    int fact=factorial(n);
-    cout<<"El factorial del numero es: "<<fact;
-
+    unsigned long long factI=factorialIterativo(n); //solo numeros positivos
+    cout<<"El factorial del numero (iterativamente)es: "<<factI<<endl;
+    unsigned long long factR=factorialRecursivo(n); //solo numeros positivos
+    cout<<"El factorial del numero (recursivamente)es: "<<factR<<endl;
     return 0;
 }   
